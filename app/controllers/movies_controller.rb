21aba225eb2f62778b3fdee5c053b0fd009f78c9
Kendra McClanahan <kendramcclanahan@kendras-mbp.att.net>
2013-12-02 10:36:51 -0600

    Add 2 video pages

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,16 @@
+class MoviesController < ApplicationController
+
+	def show_gladiator
+		@title = "Gladiator"
+		@subtitle = "Crowe shows everyone what's up"
+		@description = "Russel Crowe is a boss in this video"
+		@video_id = "//www.youtube.com/embed/FI1ylg4GKv8"
+	end
+
+	def show_matilda
+		@title = "Matilda"
+		@subtitle = "Bruce's brownie scene"
+		@description = "This movie is just great. Who agrees?"
+		@video_id = "//www.youtube.com/embed/EVWOQwZENBg"
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Metube::Application.routes.draw do
+  get 'movies/gladiator' => 'movies#show_gladiator'
+  get 'movies/matilda' => 'movies#show_matilda'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
