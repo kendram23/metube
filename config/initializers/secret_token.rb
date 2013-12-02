90cd96af069c076d56701de686dd7947a2e75227
Kendra McClanahan <kendramcclanahan@kendras-mbp.att.net>
2013-12-02 10:16:56 -0600

    Create base rails app, add vendor/bundle to gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Metube::Application.config.secret_key_base = 'df96daf8999df94d74a9537aed165555e6a097b8c36ca1f3559a99466fca60d3b5d720bcf172bffbef3ee078ec5ee03d8cf007a5240abe69ea67fed9a208c88d'
